3a6f52f44c545fa524829e8d3e592f2bb75d6d52
Raphael Antunes Baruffi <42071700+Baruffi@users.noreply.github.com>
2020-07-23 19:55:57 -0300

    simplified error handler

--- core/Handler.js
+++ core/Handler.js
@@ -1,13 +1,15 @@
-function Handler(actions, invalidAction = (identifier, action) => `Action for identifier ${identifier} has invalid type: ${typeof action}`) {
-  function handle({ identifier, context }) {
+// TODO: Improve handler adding more functionality
+function Handler(actions, unknown = null) {
+  function handle(identifier, context) {
     const action = actions[identifier];
 
     if (typeof action === 'function') {
       return action(context);
-    } else {
-      return invalidAction(identifier, action);
     }
+
+    return unknown;
   }
+
   return { handle };
 }
 
